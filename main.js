951e91673e936d4e423e00e393c6e091c5ff46e9
Arman-Markaryan <139840328+Arman-Markaryan@users.noreply.github.com>
2024-02-17 18:34:49 -0800

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ document.addEventListener('scroll', () => {
       header.classList.remove('scrolled');
     }
   });
+
+
+  window. onload = function () {
+    window. addEventListener('scroll', function (e) {
+      if (window.pageYOffset > 100) {
+          document.querySelector("header"). classList.add('is-scrolling');
+      } else {
+           document. querySelector("header").classList. remove('is-scrolling');
+      }
+  });
+
+    const menu_btn = document.querySelector('.hamburger');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+    });
+}
