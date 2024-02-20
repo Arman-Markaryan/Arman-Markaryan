e0c9d4799dd161a8c4ae61b9f9febd872ad56f12
Arman-Markaryan <139840328+Arman-Markaryan@users.noreply.github.com>
2024-02-19 19:45:22 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    if (window.scrollY > 0) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+window.onload = function () {
+  const menu_btn = document.querySelector(".hamburger");
+  const ham_menu = document.querySelector(".ham-menu");
+
+  // Toggle the menu and hamburger button states
+  function toggleMenu() {
+    menu_btn.classList.toggle("is-active");
+    ham_menu.classList.toggle("active");
+  }
+
+  // Add click event listener to the hamburger button
+  menu_btn.addEventListener("click", function () {
+    toggleMenu(); // Toggle the menu
+  });
+
+  // Add click event listeners to each menu link
+  ham_menu.addEventListener("click", function (e) {
+    // Close the menu when a link is clicked
+    if (e.target.tagName === "A") {
+      toggleMenu();
     }
   });
-
-
-  window. onload = function () {
-    window. addEventListener('scroll', function (e) {
-      if (window.pageYOffset > 100) {
-          document.querySelector("header"). classList.add('is-scrolling');
-      } else {
-           document. querySelector("header").classList. remove('is-scrolling');
-      }
-  });
-
-    const menu_btn = document.querySelector('.hamburger');
-
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active');
-    });
-}
+};
